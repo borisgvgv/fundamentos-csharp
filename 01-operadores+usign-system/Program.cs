ca87ee48e12bc3130ed54e278e4d0688218c675b
Boris Garcés <borisgarces@imac-de-boris.home>
2023-09-17 12:52:30 +0200

    Actualizar

---
+++ 01-operadores+usign-system/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(1234 + 56789);
+        Console.WriteLine(1234 - 56789);
+        Console.WriteLine(1234 * 56789);
+        Console.WriteLine(1234 / 56789); //si no se le pone .0 a uno de los dos valores devolvera 0 
+        Console.WriteLine(1234.0 / 56789);
+        Console.WriteLine(0.1234 / 56789);
+        Console.WriteLine(1234 % 56789);
+    }
+}

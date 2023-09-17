6b9b7e010c92a7fc64e1dfb240ea0979b924ea9c
Boris Garcés <borisgarces@imac-de-boris.home>
2023-09-17 19:57:52 +0200

    act

--- 05-datos-introducidos-por-el-usuario/Program.cs
+++ 05-datos-introducidos-por-el-usuario/Program.cs
@@ -5,6 +5,17 @@ class Variables
     static void Main()
     {
 
-        Console.WriteLine("hola");
+        int a = 4;
+        int b = 6;
+
+        System.Console.WriteLine("Dime el primer valor");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("Dime el segundo valor");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        int c = a + b;
+
+        System.Console.WriteLine("Resultado: " + c);
     }
 }
